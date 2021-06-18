65f4ee479a0f32310dff6547fa240b4cc11b460f
Carrington Jones <carrington.jones47@gmail.com>
2021-06-18 14:26:40 -0500

    Small updates

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,14 +1,8 @@
 USE codeup_test_db;
 
-SELECT 'Pink Floyd Albums' AS 'Pink Floyd';
-SELECT album_name FROM albums WHERE artist = 'Pink Floyd';
-SELECT 'Release Date of Sgt. Pepper' AS 'Beatles';
-SELECT release_date FROM albums WHERE album_name = 'Sgt. Pepper''s Lonely Hearts Club Band';
-SELECT 'Nirvana''s Nevermind album''s genre' AS 'Nirvana';
-SELECT genre FROM albums WHERE album_name = 'Nevermind';
-SELECT 'Albums released between 1990 and 2000' AS '1990-2000';
-SELECT album_name FROM albums WHERE release_date >= 1990 && release_date <= 2000;
-SELECT 'Albums with sales over 20 Million' AS 'Cash';
-SELECT album_name FROM albums WHERE sales = sales < 20.0;
-SELECT 'Rock Albums' AS 'Van Halen not Van Hagar';
-SELECT album_name FROM albums WHERE genre = 'Rock';
+SELECT album_name AS 'Pink Floyd Albums' FROM albums WHERE artist = 'Pink Floyd';
+SELECT release_date AS 'Release Date of Sgt. Pepper' FROM albums WHERE album_name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT genre AS'The genre for Nevermind' FROM albums WHERE album_name = 'Nevermind';
+SELECT album_name AS 'Albums released between 1990 and 2000' FROM albums WHERE release_date >= 1990 && release_date < 2000;
+SELECT album_name AS 'Albums with sales over 20 Million' FROM albums WHERE sales = sales < 20.0;
+SELECT album_name AS 'Rock Albums' FROM albums WHERE genre = 'Rock';
--- update_exercises.sql
+++ update_exercises.sql
@@ -19,15 +19,15 @@ SELECT * FROM albums WHERE artist = 'Michael Jackson';
 UPDATE albums
 SET sales = (sales * 10);
 -- WHERE id = 1;
-SELECT 'Multiplied sales by 10' AS 'Sales';
+SELECT sales FROM albums;
 
 
 UPDATE albums
 SET release_date = (release_date - 100)
 WHERE release_date < 1980;
-SELECT 'Changed all release dates before 1980 to 100 years in the past' AS 'Release Date';
+SELECT release_date FROM albums;
 
 UPDATE albums
 SET artist = 'Peter Jackson'
 WHERE artist = 'Michael Jackson';
-SELECT 'Changed Michael Jackson''s name to Peter Jackson' AS 'Peter Jackson';
+SELECT artist FROM albums WHERE artist = 'Peter Jackson';
