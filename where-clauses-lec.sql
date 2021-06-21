7af09ce70494996b3948d22d380b56fdefa6971a
Carrington Jones <carrington.jones47@gmail.com>
2021-06-21 10:44:47 -0500

    Where Clauses notes from lecture

---
+++ where-clauses-lec.sql
@@ -0,0 +1,12 @@
+USE employees;
+
+SELECT * FROM employees WHERE hire_date = '1985-01-01';
+
+SELECT first_name FROM employees WHERE first_name LIKE '%sus%';
+
+SELECT emp_no, first_name, last_name FROM employees WHERE emp_no BETWEEN 10026 AND 10082;
+-- # Don't do this:
+-- # WHERE emp_no > 10026
+-- # AND emp_no < 10082;
+
+SELECT emp_no, first_name, last_name FROM employees
