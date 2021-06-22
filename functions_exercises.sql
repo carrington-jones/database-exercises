75478562ede736a5f96c4c790358aa246aa457fa
Carrington Jones <carrington.jones47@gmail.com>
2021-06-22 09:27:28 -0500

    Completed SQL functions exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,12 +1,32 @@
 USE employees;
 
-SELECT CONCAT (first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
+SELECT CONCAT(first_name, ' ', last_name)
+FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no DESC;
 
-SELECT * FROM employees WHERE MONTH(birth_date) = 12 AND DAY(birth_date) = 25;
+SELECT *
+FROM employees
+WHERE MONTH(birth_date) = 12
+  AND DAY(birth_date) = 25;
 
-SELECT * FROM employees  WHERE YEAR(birth_date) BETWEEN 1990 AND 2000 AND MONTH(birth_date) = 12 AND DAY(birth_date) = 25;
+SELECT *
+FROM employees
+WHERE DAY(birth_date) = 25
+  AND MONTH(birth_date) = 12
+  AND YEAR(hire_date) LIKE '199%';
 
-SELECT * FROM employees  WHERE YEAR(birth_date) BETWEEN 1990 AND 2000 AND MONTH(birth_date) = 12 AND DAY(birth_date) = 25 ORDER BY birth_date DESC;
+SELECT *
+FROM employees
+WHERE DAY(birth_date) = 25
+  AND MONTH(birth_date) = 12
+  AND YEAR(hire_date) LIKE '199%'
+ORDER BY birth_date, hire_date DESC;
 
-SELECT *, DATEDIFF(CURDATE(), hire_date) AS 'Days worked at the company';
+SELECT *, DATEDIFF(hire_date, CURDATE())
+FROM employees
+WHERE (birth_date) = 25
+  AND MONTH(birth_date) = 12
+  AND YEAR(hire_date) LIKE '199%'
+ORDER BY DATEDIFF(hire_date, CURDATE()) DESC;
 
