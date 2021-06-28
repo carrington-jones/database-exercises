109fc5a0b0e46b32c6fe788e3941842186a1eafa
Carrington Jones <carrington.jones47@gmail.com>
2021-06-28 10:44:29 -0500

    Completed subqueries exercises

---
+++ subqueries_exercises.sql
@@ -0,0 +1,64 @@
+USE employees;
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date IN (
+    SELECT hire_date
+    FROM employees
+    WHERE emp_no = 101010
+);
+
+# Find all titles
+SELECT title
+FROM titles
+WHERE emp_no IN(
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+    );
+
+# Find unique titles
+SELECT DISTINCT title
+FROM titles
+WHERE emp_no IN(
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+);
+
+# Find all the current department managers that are female.
+
+SELECT first_name, last_name
+FROM employees
+WHERE gender = 'F'
+  AND emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date > NOW()
+);
+
+# Find all the department names that currently have female managers.
+SELECT dept_name
+FROM departments
+WHERE dept_no IN(
+    SELECT dept_no
+    FROM dept_manager
+    WHERE emp_no IN(
+        SELECT emp_no
+        FROM employees
+        WHERE gender = 'F'
+        AND to_date > NOW()
+        )
+    );
+
+#Find the first and last name of the employee with the highest salary.
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no = (
+    SELECT emp_no
+    FROM salaries
+    WHERE salary = (
+        SELECT MAX(salary)
+        FROM salaries
+        )
+    );
